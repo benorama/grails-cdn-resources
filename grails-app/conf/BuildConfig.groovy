24adadf47af7fddd36e7f520aac50d62537a0a10
Benoit Hediard <ben@agorapulse.com>
2013-04-18 14:41:49 +0200

    Grails upgraded to 2.2.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,7 @@ grails.project.dependency.resolution = {
         compile('org.codehaus.groovy.modules.http-builder:http-builder:0.6') {
             excludes "commons-logging", "xml-apis", "groovy"
         }
+        test 'org.spockframework:spock-grails-support:0.7-groovy-2.0'
     }
     plugins {
         build(':release:2.2.0', ':rest-client-builder:1.0.3') {
@@ -20,7 +21,19 @@ grails.project.dependency.resolution = {
         }
         runtime ':resources:1.2.RC2'
         test(':spock:0.7') {
+            exclude 'spock-grails-support'
             export = false
         }
+    }
+}
+
+grails.project.dependency.distribution = {
+    // To deploy run "grails maven-deploy --repository=snapshots"
+    remoteRepository(id: "snapshots", url: "http://repository.agorapulse.com/nexus/content/repositories/snapshots/") {
+        authentication username: "deployment", password: "eej-yoylm-of-cev"
+    }
+    // To deploy run "grails maven-deploy --repository=releases"
+    remoteRepository(id: "releases", url: "http://repository.agorapulse.com/nexus/content/repositories/releases/") {
+        authentication username: "deployment", password: "eej-yoylm-of-cev"
     }
 }
